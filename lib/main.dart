cff19cb543bb3df922c8b6cf400d1799784e5321
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2022-01-15 11:42:29 -0300

    add firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:cuidaper_mobile/app/app_module.dart';
 import 'package:cuidaper_mobile/app/app_widget.dart';
+import 'package:cuidaper_mobile/app/core/application_start_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+Future<void> main() async {
+  await ApplicationStartConfig().configureApp();
   runApp(ModularApp(
     module: AppModule(),
     child: const AppWidget(),
